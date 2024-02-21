9c33335be88c35c4bf812b57e2855432e8187275
BinyK <u1445181@utah.edu>
2024-02-20 23:20:00 -0700

    range (1, 4) create option 1, 2, and 3. And every option will change words with given file from Canvas. And finish the code with "__main__"

--- SpinFile.py
+++ SpinFile.py
@@ -10,3 +10,11 @@ def main():
     spinner = Spinner()
 
     print("Original :", original_text)
+
+    for i in range(1, 4):
+        changed_text = spinner.change_words(original_text)
+
+        print("Option", i, ":", changed_text)
+
+if __name__ == "__main__":
+    main()
