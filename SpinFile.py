86acf2c31cece1d09717c82adcebc02695a05c5b
BinyK <159490321+BinyK@users.noreply.github.com>
2024-02-20 23:13:15 -0700

    Restart

--- SpinFile.py
+++ SpinFile.py
@@ -1,21 +1,4 @@
 # https://github.com/BinyK/A4-Word-Spinner.git
 # Eunbin Kang
 
-from Spinner import Spinner
 
-def main():
-    with open('essay.txt', 'r') as file:
-        original_text = file.read()
-
-    spinner = Spinner()
-
-    print("Original :", original_text)
-
-    for i in range(1, 4):
-        changed_text = spinner.change_words(original_text)
-
-        print("Option", i, ":", changed_text)
-
-
-if __name__ == "__main__":
-    main()
