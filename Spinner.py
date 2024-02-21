d698d8c6b5ae0bb20d37c891e734b30256e93471
BinyK <159490321+BinyK@users.noreply.github.com>
2024-02-20 23:30:31 -0700

    Update Spinner.py
    Import random to start code, and the class is Spinner. Code needs to load synonyms from a given text file.
    
    I need to use strip and split to make blanks between words.

--- Spinner.py
+++ Spinner.py
@@ -3,3 +3,11 @@ import random
 class Spinner:
     def __init__(self):
         self.synonyms = self.load_synonyms('synonyms-simplified.txt')
+
+    def load_synonyms(self, filename):
+        synonyms = {}
+        with open(filename, 'r') as file:
+            for line in file:
+                word, synonyms_str = line.strip().split(':')  # strip & split 사용
+                synonyms[word] = synonyms_str.split(',')
+        return synonyms
